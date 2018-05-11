ca37a9eef7ad212f2944f3c49c16e57d4d5414e8
aswinwahyudi31 <aswin.wahyudi1@gmail.com>
2018-05-11 16:37:57 +0700

    Menambah Class Dadu

---
+++ src/main/java/sqa/joss/tugasantara/TugasantaraApplication.java
@@ -0,0 +1,12 @@
+package sqa.joss.tugasantara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TugasantaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TugasantaraApplication.class, args);
+	}
+}
