3ba16e485df05a41f0d4e9a3e8cf573f34bb0789
samuelindraw <samuel_wijaya75@yahoo.co.id>
2018-05-12 01:12:28 -0700

    menyesuaikan codingan dengan aswin

--- src/main/java/sqa/joss/tugasantara/Dadu.java
+++ src/main/java/sqa/joss/tugasantara/Dadu.java
@@ -1,6 +1,10 @@
 package sqa.joss.tugasantara;
 
+import javax.management.StringValueExp;
 import javax.swing.*;
+
+import java.awt.Color;
+import java.awt.Graphics;
 import java.util.Random;
 
 public final class Dadu extends JFrame {
@@ -13,13 +17,13 @@ public final class Dadu extends JFrame {
 
     public Dadu() {
         this(DEFAULT_SISI);
-        setSize(400,500);
+        setSize(260,165);
         setVisible(true);
     }
 
     public Dadu(int numSides) {
         this.jumSisi = numSides;
-        setSize(400,500);
+        setSize(260,165);
         setVisible(true);
     }
 
@@ -41,4 +45,27 @@ public final class Dadu extends JFrame {
     public String toString() {
         return "Jumlah sisi dadu: " + getJumSisi() + " hasil: " + getHasil();
     }
+    public void paint(Graphics g) {
+    	super.paint(g);
+    	int randomnumber, randomnumber2, randomnumber3;
+    	randomnumber = randGenerator.nextInt(255);
+    	randomnumber2 = randGenerator.nextInt(255);
+    	randomnumber3 = randGenerator.nextInt(255);
+    	
+    	Color myWhite = new Color(randomnumber,randomnumber2,randomnumber3);//color random 
+    	g.drawString(String.valueOf(randomnumber),77,120);
+    	g.drawString(String.valueOf(randomnumber2),107,220);
+    	g.drawString(String.valueOf(randomnumber3),137,120);
+    	
+    	
+    	g.setColor(myWhite);
+    	g.fillRect(20, 40, 220, 50);
+    	
+    	g.setColor(Color.BLUE);
+    	g.drawRect(20,100,220,50);
+    	
+    	g.setColor(Color.RED);
+    	g.drawString(toString(),57,60);
+    }
 }
+
--- src/main/java/sqa/joss/tugasantara/TugasantaraApplication.java
+++ src/main/java/sqa/joss/tugasantara/TugasantaraApplication.java
@@ -1,5 +1,7 @@
 package sqa.joss.tugasantara;
 
+import javax.swing.JFrame;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,12 @@ public class TugasantaraApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TugasantaraApplication.class, args);
+		
+		Dadu dadu = new Dadu(8);
+		dadu.lempar();
+		
+		dadu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 	}
+	
 }
